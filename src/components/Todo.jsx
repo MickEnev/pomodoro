e55c512f8ae117881813e5064f3e72983646ce2f
MickEnev <mickenev@gmail.com>
2025-04-05 17:05:52 -0500

    tired

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,11 +4,17 @@ import '../css/Todo.css'
 export default function Todo() {
   return (
     <div className="todo">
+      {/*TODO: 
+      Be able to add tasks 
+      Be able to mark tasks as done which removes them from the list
+      ----------------  MVP  -------------
+      Store complted tasks
+      Be able to delete tasks rather than mark them complete*/}
       <ul className="todo-list">
-      <li className="task">Balls</li>
-      <li className="task">Balls</li>
-      <li className="task">Balls</li>
-      <li className="task">Balls</li>
+        <li className="task">BallsBalls</li>
+        <li className="task">Balls</li>
+        <li className="task">Balls</li>
+        <li className="task">Balls</li>
       </ul>
     </div>
   )
